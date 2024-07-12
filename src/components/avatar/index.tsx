709bc91ef538071fcf99c680a134d889f41d455e
Kaique <kaiquesimoes58@gmail.com>
2024-07-12 13:15:51 -0300

    Estilizando a sidebar

---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/KaikalDev.png" />
+
+export default Avatar
--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/avatar'
+import Paragrafo from '../../components/Paragrafos'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/KaikalDev.png" />
-    <Titulo fontSize={20}>Kaique Luan</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Kaique Luan</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        KaikalDev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor full-stack Java
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
